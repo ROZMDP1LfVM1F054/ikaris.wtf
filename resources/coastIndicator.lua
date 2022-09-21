0e9216afbaac9a0285922ddf121a24e243eba862
auonn <101582215+auonn@users.noreply.github.com>
2022-09-21 14:50:17 -0400

    Update coastIndicator.lua

--- resources/coastIndicator.lua
+++ resources/coastIndicator.lua
@@ -141,7 +141,7 @@ function lib:newIndicator(title)
         StatsTitle1.TextSize = 19.000
         StatsTitle1.TextWrapped = true
         StatsTitle1.TextXAlignment = Enum.TextXAlignment.Left
-        return StatsTitle, StatsTitle1
+        return StatsTitle1
     end
     return libaray
 end
